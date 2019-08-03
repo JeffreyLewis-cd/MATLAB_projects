c2f5ff7e4958a7273e61ceafa626fa281f96aabf
JiangFan6 <jiangfan6@outlook.com>
2019-08-03 12:54:03 +0800

    show the position of eyes

--- gabor_function/26Master2009/fillRect.m
+++ gabor_function/26Master2009/fillRect.m
@@ -1,3 +1,6 @@
+%show the position of eyes
+
+%method 1
 im = imread('KA.AN1.39.tiff');
 subplot(1,2,1)
 imshow(im);
@@ -5,6 +8,7 @@ hold on;
 fill([75,185,185,75,75],[120,120,140,140,120],'w','edgeColor','w','facealpha',0);
 
 
+%method2
 subplot(1,2,2)
 imshow(uint8(im));
 
