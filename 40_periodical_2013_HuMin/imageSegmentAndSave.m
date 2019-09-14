5cf01c8fe0758a1ad210770ba38230322483d541
JiangFan6 <jiangfan6@outlook.com>
2019-09-14 09:47:29 +0800

    add two face sets to test face detect function

--- 40_periodical_2013_HuMin/imageSegmentAndSave.m
+++ 40_periodical_2013_HuMin/imageSegmentAndSave.m
@@ -1,6 +1,6 @@
 clear;
-stImageFilePath  = 'D:\backupData\JiangFanBackup_Tech\videoImage\master\MATLAB_projects\40_periodical_2013_HuMin\';
-stImageSavePath  = 'D:\backupData\JiangFanBackup_Tech\videoImage\master\MATLAB_projects\40_periodical_2013_HuMin\';
+stImageFilePath  = 'D:\backupData\JiangFanBackup_Tech\videoImage\master\MATLAB_projects\40_periodical_2013_HuMin\KL\';
+stImageSavePath  = 'D:\backupData\JiangFanBackup_Tech\videoImage\master\MATLAB_projects\40_periodical_2013_HuMin\KL_face_only\';
 dirImagePathList = dir(strcat(stImageFilePath,'*.tiff'));        %读取该目录下全部图片的路径（字符串格式）
 iImageNum        = length(dirImagePathList);                    %获取图片的总数量
 if iImageNum > 0                                                %批量读入图片，进行五官检測，再批量检測
